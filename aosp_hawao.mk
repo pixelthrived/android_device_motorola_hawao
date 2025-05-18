383d9c29ba0acc1ec58be3e3d09ef791beb2fb81
PixelThrived <91941157+pixelthrived@users.noreply.github.com>
2025-05-18 20:58:07 +0300

    TPP: bring-up for hawao
    Change-Id: Icc06265a729dcecd6eff155c3a0c25d5509a5bf8

--- aosp_hawao.mk
+++ aosp_hawao.mk
@@ -10,11 +10,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/motorola/hawao/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common TPP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# The Pixel Project specific variables
+TARGET_BOOT_ANIMATION_RES := 1080
+CUSTOM_MAINTAINER := PixelThrived
+#TARGET_ENABLE_BLUR := false # God forbid you even THINK of setting this variable to true!
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_STOCK_AICORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_hawao
+PRODUCT_NAME := aosp_hawao
 PRODUCT_DEVICE := hawao
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g42
